9923739be5d53f4060c9c491b514d6467396bdc2
Oinant <antoine.sauvinet@gmail.com>
2015-04-22 15:04:14 +0200

    Initial

---
+++ FruitShop.Tests/UnitTest1.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace FruitShop.Tests
+{
+    [TestClass]
+    public class CashierTests  
+    {
+        [TestMethod]
+        public void WhenInputApples_ThenOutput100()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Apples").ShouldBe(100);
+        }
+
+        [TestMethod]
+        public void ApplesThenCherries_Output175()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Apples").ShouldBe(100);
+            cashier.Enter("Cherries").ShouldBe(175);
+        }
+
+        [TestMethod]
+        public void ApplesThenCherriesThenCherries_Output230_withDiscount()
+        {
+            var cashier = new Cashier();
+            var result = cashier.Enter("Apples");
+            result = cashier.Enter("Cherries");
+            result = cashier.Enter("Cherries");
+            result.ShouldBe(230);
+
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Cherries");
+            cashier.Enter("Apples");
+            cashier.Enter("Cherries");
+            cashier.Enter("Bananas");
+            cashier.Enter("Cherries");
+            cashier.Enter("Cherries");
+            var result = cashier.Enter("Apples");
+            
+            result.ShouldBe(410);
+        }
+
+        [TestMethod]
+        public void TestFreeBananas()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Cherries");
+            cashier.Enter("Cherries");
+            cashier.Enter("Bananas");
+            var result = cashier.Enter("Bananas");
+            result.ShouldBe(280);
+        }
+
+
+        [TestMethod]
+        public void TestFreeBananas2()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Cherries");
+            cashier.Enter("Cherries");
+            cashier.Enter("Cherries");
+            cashier.Enter("Bananas");
+            cashier.Enter("Apples");
+            cashier.Enter("Apples");
+            var result = cashier.Enter("Bananas");
+            result.ShouldBe(355);
+        }
+        
+        [TestMethod]
+        public void WhenInputApples_ThenOutput100_Localisation()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Apples").ShouldBe(100);
+            cashier.Enter("Pommes").ShouldBe(200);
+            cashier.Enter("Mele").ShouldBe(300);
+        }
+
+        [TestMethod]
+        public void WhenInputApples_ThenOutput100_LocalisationFull()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Cherries").ShouldBe(75);
+            cashier.Enter("Pommes").ShouldBe(175);
+            cashier.Enter("Cherries").ShouldBe(230);
+            cashier.Enter("Bananas").ShouldBe(380);
+            cashier.Enter("Apples").ShouldBe(280);
+            cashier.Enter("Mele").ShouldBe(380);
+        }
+
+        [TestMethod]
+        public void LocalizedPromotions()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Mele").ShouldBe(100);
+            cashier.Enter("Pommes").ShouldBe(200);
+            cashier.Enter("Pommes").ShouldBe(300);
+            cashier.Enter("Apples").ShouldBe(300);
+            cashier.Enter("Pommes").ShouldBe(100);
+            cashier.Enter("Mele").ShouldBe(100);
+            cashier.Enter("Cherries").ShouldBe(175);
+            cashier.Enter("Cherries").ShouldBe(230);
+
+        }
+
+        [TestMethod]
+        public void TestCSV()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Mele, Pommes, Pommes, Apples, Pommes, Mele, Cherries, Cherries, Bananas").ShouldBe(380);
+        }
+
+        [TestMethod]
+        public void Test_HotDeal1()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Mele, Pommes, Pommes, Mele").ShouldBe(200);
+            cashier.Enter("Bananas").ShouldBe(150);
+            cashier.Enter("Mele, Pommes, Pommes, Apples, Mele").ShouldBe(150);
+        }
+
+        [TestMethod]
+        public void Test_HotDeal2()
+        {
+            var cashier = new Cashier();
+            cashier.Enter("Mele, Pommes, Apples, Pommes, Mele").ShouldBe(100);
+            cashier.Enter("Bananas").ShouldBe(250);
+        }
+    }
+}
---
+++ FruitShop/Cashier.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FruitShop
+{
+    public class Cashier
+    {
+        private int _total = 0;
+        private int cherryPackCount = 0;
+        private int bananasPackCount = 0;
+
+        private int pommesPackCount = 0;
+        private int melePackCount = 0;
+        private int applessPackCount = 0;
+
+        private int fruitsPackCount;
+
+        public int Enter(string fruits)
+        {
+            foreach (var fruit in fruits.Split(new []{", "}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                EnterSingle(fruit);
+            }
+            return _total;
+        }
+
+        readonly List< string> applenames = new List<string>(){"Apples", "Pommes", "Mele"}; 
+
+        private int EnterSingle(string fruit)
+        {
+            if (string.Equals(fruit, "Apples", StringComparison.OrdinalIgnoreCase))
+            {
+                fruitsPackCount++;
+                _total += 100;
+                applessPackCount++;
+               
+            }
+            else if (string.Equals(fruit, "Mele", StringComparison.OrdinalIgnoreCase))
+            {
+                fruitsPackCount++;
+                _total += 100;
+                applessPackCount++;
+                melePackCount++;
+                if (melePackCount == 2)
+                {
+                    _total -= 100;
+                    melePackCount = 0;
+                }
+            }
+            else if (string.Equals(fruit, "Pommes", StringComparison.OrdinalIgnoreCase))
+            {
+                fruitsPackCount++;
+                _total += 100;
+                applessPackCount++;
+                pommesPackCount++;
+                if (pommesPackCount == 3)
+                {
+                    _total -= 100;
+                    pommesPackCount = 0;
+                }
+            }
+            else if (string.Equals(fruit, "Cherries", StringComparison.OrdinalIgnoreCase))
+            {
+                fruitsPackCount++;
+                _total += 75;
+                cherryPackCount++;
+                if (cherryPackCount == 2)
+                {
+                    _total -= 20;
+                    cherryPackCount = 0;
+                }
+            }
+            else if (string.Equals(fruit, "Bananas", StringComparison.OrdinalIgnoreCase))
+            {
+                fruitsPackCount++;
+                _total += 150;
+
+                bananasPackCount++;
+                if (bananasPackCount == 2)
+                {
+                    _total -= 150;
+                    bananasPackCount = 0;
+                }
+            }
+
+            if (applessPackCount == 4)
+            {
+                _total -= 100;
+                applessPackCount = 0;
+            }
+
+            if (fruitsPackCount == 5)
+            {
+                _total -= 200;
+                fruitsPackCount = 0;
+            }
+               
+            return _total;
+        }
+
+        
+    }
+}
